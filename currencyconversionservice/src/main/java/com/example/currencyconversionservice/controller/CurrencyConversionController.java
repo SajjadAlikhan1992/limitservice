c36b9a584d075c52192ad08143f38a6364444312
Sajjad Alikhan <navabsajjad.ali@gmail.com>
2023-06-19 17:57:07 +0530

    updated new files

--- currencyconversionservice/src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
+++ currencyconversionservice/src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.currencyconversionservice.domain.CurrencyConversion;
+import com.example.currencyconversionservice.feign.services.CurrrencyExchangeProxy;
 import com.example.currencyconversionservice.services.CurrencyConversionServices;
 
 @RestController
@@ -19,6 +20,9 @@ public class CurrencyConversionController {
 
 	@Autowired
 	private CurrencyConversionServices currencyConversionServices;
+
+	@Autowired
+	private CurrrencyExchangeProxy currencyExchangeProxy;
 
 	@Autowired
 	private Environment environment;
@@ -31,9 +35,25 @@ public class CurrencyConversionController {
 		logger.info("fromCurrency -> " + fromCurrency + "toCurrency ->" + toCurrency + "and Quantity ->" + quantity);
 		CurrencyConversion currencyConversion = currencyConversionServices.fetchCurrencyConversion(fromCurrency,
 				toCurrency, quantity);
-		currencyConversion.setEnvironment(environment.getProperty("local.server.port"));
+		currencyConversion.setEnvironment(environment.getProperty("local.server.port") + " Rest Template ");
 		ResponseEntity<CurrencyConversion> responseEntity = new ResponseEntity<CurrencyConversion>(currencyConversion,
 				HttpStatus.OK);
 		return responseEntity;
 	}
+
+	@GetMapping("/currencyconversion-feign/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
+	public ResponseEntity<CurrencyConversion> fetchCurrencyConversionbyFeign(
+			@PathVariable("fromCurrency") String fromCurrency, @PathVariable("toCurrency") String toCurrency,
+			@PathVariable("quantity") BigDecimal quantity) {
+		logger.info("fromCurrency -> " + fromCurrency + "toCurrency ->" + toCurrency + "and Quantity ->" + quantity);
+
+		CurrencyConversion currencyConversion = currencyExchangeProxy
+				.fetchExchangeRatesBasedOnFromAndToCurreny(fromCurrency, toCurrency);
+		currencyConversion.setQuantity(quantity);
+		currencyConversion.setTotalCaluculatedAmount(quantity.multiply(currencyConversion.getConversionMultiples()));
+		currencyConversion.setEnvironment(environment.getProperty("local.server.port") + " feign ");
+
+		return ResponseEntity.ok(currencyConversion);
+	}
+
 }
